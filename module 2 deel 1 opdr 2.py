118974c0f742651f5b2b17397daec6162fc5e198
Timurr32 <99083657@mydavinci.nl>
2025-01-04 01:30:29 +0100

    opdr 1 tot 3

---
+++ module 2 deel 1 opdr 2.py
@@ -0,0 +1,5 @@
+import time
+for x in range(30, -1, -1):
+    print(x)
+    time.sleep(1)
+print("Raket gelanceerd!")
---
+++ module 2 deel 1 opdr 3.py
@@ -0,0 +1,3 @@
+for x in ['AM', 'PM']:
+    for hour in range(1, 13): 
+            print(f"{hour} {x}")
