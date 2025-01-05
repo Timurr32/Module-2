6867fce4758e028ff50ea160b89b6eb61549b333
Timurr32 <99083657@mydavinci.nl>
2025-01-05 20:42:39 +0100

    Create module 2 deel 3 opdr 3.py

---
+++ module 2 deel 3 opdr 3.py
@@ -0,0 +1,15 @@
+import time
+
+rood = 20
+groen = 30
+oranje = 10
+
+while True: 
+    print("🚦 Stoplicht is ROOD!")
+    time.sleep(rood)
+    
+    print("🚦 Stoplicht is GROEN!")
+    time.sleep(groen)
+    
+    print("🚦 Stoplicht is ORANJE!")
+    time.sleep(oranje)
