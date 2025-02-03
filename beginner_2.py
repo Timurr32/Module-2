e07ae11e860f0ca165c87d97336083a007f43898
Timurr32 <99083657@mydavinci.nl>
2025-02-03 10:20:13 +0100

    m

---
+++ beginner_2.py
@@ -0,0 +1,40 @@
+from RobotArm import RobotArm
+
+# Import the challenges (in this case challenges/example.py)
+from challenges.beginner import challenges
+
+# load the robotarm with a challenge on a level (max 3)
+robotArm = RobotArm(challenges[2],0)
+
+# your code starts here:
+robotArm.moveRight()
+robotArm.grab()
+def box_replace():
+    color = robotArm.scan()
+    if color == "green":
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveLeft()
+        robotArm.grab()
+    elif color == 'blue':
+        robotArm.moveLeft()
+        robotArm.drop()
+        robotArm.moveRight()
+        robotArm.grab()
+        box_replace()
+        return
+for i in range(6):
+    box_replace()
+
+# your code ends here
+
+# report the results of the mission
+robotArm.report()
+
+# want help? Unlock code below!
+robotArm.help()
+
+# want to inspect a solution? Unlock code below!
+# robotArm.showSolution()
+# robotArm.wait()
+
